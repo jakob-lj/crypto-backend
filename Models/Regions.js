cea6d8750cdf2de1bd4d4c93b1d3c7db61299e7e
Jakob Lund Johannessen <jakob-lj@users.noreply.github.com>
2019-01-19 22:23:36 +0100

    Delete Regions.js

--- Models/Regions.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const user = require('./User').userScheme;
-const RegionSchema = new Schema({
-  name:String,
-  user: [user] 
-});
-
-mongoose.model('Region', RegionSchema);
